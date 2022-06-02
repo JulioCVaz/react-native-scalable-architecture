07d7f187dedfcdfe4f8f19678d28508d4084f03f
Juliovaz <julio.oliveiravaz0@gmail.com>
2022-06-02 01:00:06 -0300

    feat(hashira): implements codepush in hashira app

--- apps/hashira/src/main.tsx
+++ apps/hashira/src/main.tsx
@@ -1,4 +1,12 @@
+import { FunctionComponent } from 'react';
 import { AppRegistry } from 'react-native';
+import CodePush from 'react-native-code-push';
 import App from './app/App';
 
-AppRegistry.registerComponent('Hashira', () => App);
+let codePushOptions = {
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+};
+
+let HashiraApp: () => FunctionComponent = () => CodePush(codePushOptions)(App);
+
+AppRegistry.registerComponent('Hashira', CodePush(codePushOptions)(HashiraApp));
