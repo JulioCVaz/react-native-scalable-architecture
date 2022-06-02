0749e834af61143a93d5084a89db163a3e989ccb
Juliovaz <julio.oliveiravaz0@gmail.com>
2022-06-02 01:00:27 -0300

    feat(tomioka): implements codepush in tomioka app

--- apps/tomioka/src/main.tsx
+++ apps/tomioka/src/main.tsx
@@ -1,4 +1,12 @@
+import { FunctionComponent } from 'react';
 import { AppRegistry } from 'react-native';
+import CodePush from 'react-native-code-push';
 import App from './app/App';
 
-AppRegistry.registerComponent('Tomioka', () => App);
+let codePushOptions = {
+  checkFrequency: CodePush.CheckFrequency.ON_APP_RESUME,
+};
+
+let TomiokaApp: () => FunctionComponent = () => CodePush(codePushOptions)(App);
+
+AppRegistry.registerComponent('Tomioka', TomiokaApp);
